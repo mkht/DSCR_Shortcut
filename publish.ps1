7b44123412ed6a1aca0c76084fad2b724fdee5aa
mkht <mkht@users.noreply.github.com>
2018-07-25 00:04:49 +0900

    Add publish script for PSGallery

---
+++ publish.ps1
@@ -0,0 +1,33 @@
+Param(
+    [Parameter(Mandatory = $true)]
+    [string]$NugetApiKey,
+
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string[]]$ExcludeDir = @('.git'),
+
+    [switch]$WhatIf
+)
+
+$ModuleDir = $PSScriptRoot
+$ModuleName = Split-Path $ModuleDir -Leaf
+$Destination = Join-Path $env:TEMP $ModuleName
+
+if (Test-Path $Destination) {
+    Remove-Item $Destination -Force -Recurse -ErrorAction Stop
+}
+
+if ($ExcludeDir -notcontains '.git') {
+    $ExcludeDir += '.git'
+}
+
+try {
+    robocopy $ModuleDir $Destination /MIR /XD ($ExcludeDir -join ' ') /XF "publish.ps1" >$null
+
+    Set-Location $Destination
+    Publish-Module -Path ./ -NuGetApiKey $NugetApiKey -Verbose -WhatIf:$WhatIf
+}
+finally {
+    Set-Location $ModuleDir
+    Remove-Item $Destination -Force -Recurse -ErrorAction Continue
+}
